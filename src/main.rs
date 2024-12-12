e10ccf22650aee8bb0162ecfe3c0661ff4290d6e
Mattia Fiumara <mfiumara@protonmail.com>
2024-12-12 21:31:36 +0100

    wipg

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 #![no_std]
 #![no_main]
 
+use defmt::info;
 use embassy_executor::Spawner;
 use embassy_nrf::gpio::{Level, Output, OutputDrive};
 use embassy_time::Timer;
@@ -9,11 +10,13 @@ use {defmt_rtt as _, panic_probe as _};
 #[embassy_executor::main]
 async fn main(_spawner: Spawner) {
     let p = embassy_nrf::init(Default::default());
-    let mut led = Output::new(p.P0_02, Level::Low, OutputDrive::Standard);
+    let mut led = Output::new(p.P0_31, Level::Low, OutputDrive::HighDrive);
 
     loop {
         led.set_high();
+        info!("Hi");
         Timer::after_millis(300).await;
+        info!("Low");
         led.set_low();
         Timer::after_millis(300).await;
     }
